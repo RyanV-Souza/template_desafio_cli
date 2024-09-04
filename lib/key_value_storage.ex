3f5229af98d54cfea50bcb0c138713345e925649
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-09-04 09:42:45 -0300

    doc: type functions

--- lib/key_value_storage.ex
+++ lib/key_value_storage.ex
@@ -1,12 +1,31 @@
 defmodule KeyValueStorage do
- defstruct db: %{}, transactions: []
+  @moduledoc """
+  Represents a key-value storage.
+  """
 
- def new() do
-   %KeyValueStorage{}
- end
+  defstruct db: %{}, transactions: []
 
- def get(%KeyValueStorage{db: db}, key) do
-  Map.get(db, key, "NIL")
- end
+  @type key :: String.t()
+  @type value :: integer() | String.t() | boolean()
+  @type db :: %{optional(key) => value}
+  @type transaction :: %{optional(key) => value}
+  @type t :: %KeyValueStorage{
+          db: db(),
+          transactions: [transaction()]
+        }
 
+  @doc """
+  Creates a new key-value storage.
+  """
+  def new() do
+    %KeyValueStorage{}
+  end
+
+  @doc """
+  Gets the value of a key in the storage.
+  """
+  @spec get(t(), key()) :: value() | String.t()
+  def get(%KeyValueStorage{db: db}, key) do
+    Map.get(db, key, "NIL")
+  end
 end
