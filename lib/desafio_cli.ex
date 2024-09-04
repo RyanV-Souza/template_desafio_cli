4fb704771c1f27f73b5bfd387bd26d62a3e2fdc0
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-09-04 08:55:06 -0300

    feat: implements a storage loop with get method

--- lib/desafio_cli.ex
+++ lib/desafio_cli.ex
@@ -1,17 +1,32 @@
 defmodule DesafioCli do
-  @moduledoc """
-  Ponto de entrada para a CLI.
-  """
 
-  @doc """
-  A função main recebe os argumentos passados na linha de
-  comando como lista de strings e executa a CLI.
-  """
   def main(_args) do
-    IO.puts(phrase())
+    IO.puts("[Desafio] Banco de dados chave-valor")
+    storage = KeyValueStorage.new()
+    loop(storage)
   end
 
-  def phrase() do
-    "Hello, world!"
+  def loop(storage) do
+    IO.write("> ")
+    input = IO.gets("") |> String.trim()
+
+    case parse_input(input) do
+      {:get, key} ->
+        value = KeyValueStorage.get(storage, key)
+        IO.puts(value)
+        loop(storage)
+
+      _ -> :error
+        IO.puts("ERR \"No command #{input}\"")
+        loop(storage)
+    end
+
+  end
+
+  defp parse_input(input) do
+    case String.split(input) do
+      ["GET", key] -> {:get, key}
+      _ -> :error
+    end
   end
 end
