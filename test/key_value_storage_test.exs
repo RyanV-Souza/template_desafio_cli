0992d86c678908bcbbef386ba6d8769caf0f2bfc
Ryan Vieira Souza <ryanvsouza99@gmail.com>
2024-09-05 12:33:29 -0300

    test: add tests for set/3 to handle key-value setting and updates

--- test/key_value_storage_test.exs
+++ test/key_value_storage_test.exs
@@ -10,4 +10,30 @@ defmodule KeyValueStorageTest do
       assert storage.transactions == []
     end
   end
+
+  describe "set/3" do
+    test "sets a value in the storage db without transactions" do
+      storage = KeyValueStorage.new()
+      {existed, new_storage} = KeyValueStorage.set(storage, "my_key", "my_value")
+
+      assert existed == false
+      assert new_storage.db == %{"my_key" => "my_value"}
+    end
+
+    test "updates an existing key in the storage db" do
+      storage = KeyValueStorage.new() |> KeyValueStorage.set("my_key", "old_value") |> elem(1)
+      {existed, new_storage} = KeyValueStorage.set(storage, "my_key", "new_value")
+
+      assert existed == true
+      assert new_storage.db == %{"my_key" => "new_value"}
+    end
+
+    test "sets a value inside an active transaction" do
+      storage = KeyValueStorage.new() |> KeyValueStorage.begin()
+      {existed, new_storage} = KeyValueStorage.set(storage, "my_key", "my_value")
+
+      assert existed == false
+      assert hd(new_storage.transactions) == %{"my_key" => "my_value"}
+    end
+  end
 end
